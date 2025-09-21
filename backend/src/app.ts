3bda7c37490ce6b81fd0267614b7bd46a35ad6f4
gbatistaa <gbatistadev@gmail.com>
2025-09-21 11:47:18 -0300

    feat(backend-crud): create the server main typescript file

---
+++ backend/src/app.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import conn from "./database/database";
+
+const app = express();
+
+app.use(express.json());
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+);
+
+app.use(express.json());
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Backend running on port: ${PORT}`);
+});
+
+const syncDatabase = async (): Promise<void> => {
+  try {
+    console.clear();
+
+    await conn.sync({ force: true }); // Only for development phase
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+void syncDatabase();
+
+export default app;

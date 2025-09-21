c2d7c3c39b84303211de42194da1d8ce0e13abf2
gbatistaa <gbatistadev@gmail.com>
2025-09-21 20:48:04 -0300

    chore(products-crud): clean the comments of the product validator file

--- backend/src/validator/product.validator.ts
+++ backend/src/validator/product.validator.ts
@@ -1,17 +1,16 @@
-// src/validations/product.validation.ts
 import { z } from "zod";
 
-// Schema for validating the request params
+// Schema for validating the request params:
 export const paramsSchema = z.object({
   id: z.uuid("Id must be a valid UUIDv4"),
 });
 
-// Schema for creating a product:
+// Schema for request body on creating a product:
 export const createProductSchema = z.object({
   name: z.string("Name is required").min(3, "Name must be at least 3 characters long"),
   description: z.string("Description is required"),
   price: z.number("Price is required").positive("Price must be a positive number"),
 });
 
-// Schema for updating a product (all fields are optional)
+// Schema for updating a product:
 export const updateProductSchema = createProductSchema.partial();

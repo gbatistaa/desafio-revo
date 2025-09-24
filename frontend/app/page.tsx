eea4830a5242e98ab9025be205ff872ab2243890
gbatistaa <gbatistadev@gmail.com>
2025-09-24 00:21:02 -0300

    feat(frontend-ui): create the create or edit products form react component

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,15 +1,19 @@
 "use client";
 
+import ProductForm from "./components/CreateProductForm";
 import Header from "./components/Header";
 import ProductsTable from "./components/ProductsTable";
 import Search from "./components/Search";
 
 export default function App() {
   return (
-    <div className="flex flex-col gap-8 font-sans min-h-screen px-40 py-12">
-      <Header />
-      <Search />
-      <ProductsTable />
-    </div>
+    <>
+      <div className="flex flex-col gap-8 font-sans min-h-screen min-w-screen px-40 py-12 absolute z-10">
+        <Header />
+        <Search />
+        <ProductsTable />
+      </div>
+      <ProductForm />
+    </>
   );
 }
